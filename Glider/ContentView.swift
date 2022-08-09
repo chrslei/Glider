8946e7dbf0badaf7443f9e72b3503a8de4ab097c
Christopher <chrscao1@gmail.com>
2022-08-09 16:11:01 +0200

    Initial Commit

---
+++ Glider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glider
+//
+//  Created by Christopher Leibiger on 09.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glider/GliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GliderApp.swift
+//  Glider
+//
+//  Created by Christopher Leibiger on 09.08.22.
+//
+
+import SwiftUI
+
+@main
+struct GliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
